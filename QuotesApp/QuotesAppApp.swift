69d8252069e4312f72bde0bc3319a649e04f0f0a
ErenUnal24 <bilalunal24@gmail.com>
2024-05-31 14:33:28 +0300

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by Eren on 31.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
